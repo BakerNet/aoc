ef413c480704dc13e5b87b4266120eecf4c606cc
BakerNet <hansbaker90@gmail.com>
2025-12-07 01:47:39 -0800

    Optimization for funsies

--- aoc2025/src/bin/07.rs
+++ aoc2025/src/bin/07.rs
@@ -24,26 +24,61 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let map = input.c_map();
-    let mut map_counts = vec![vec![0; map[0].len()]; map.len()];
-    for r in 0..map.len() {
-        for c in 0..map[r].len() {
-            let prev = map_counts[if r > 0 { r - 1 } else { 0 }][c];
-            let curr = map[r][c];
-            match (prev, curr) {
-                (_, 'S') => map_counts[r][c] = 1,
-                (x, '.') if x > 0 => {
-                    map_counts[r][c] += map_counts[r - 1][c];
-                }
-                (x, '^') if x > 0 => {
-                    map_counts[r][c - 1] += map_counts[r - 1][c];
-                    map_counts[r][c + 1] += map_counts[r - 1][c];
+    // zero alocations for ZOOM ZOOM
+    let bytes = input.as_bytes();
+    let width = input.find('\n').unwrap();
+    let mut map_counts = vec![0; width];
+    let s_loc = width / 2; // starting location of 'S'
+    map_counts[s_loc] = 1;
+    let mut skip = s_loc;
+    for row in bytes.chunks(width + 1).step_by(2).skip(1) {
+        skip -= 1;
+        for c in skip..width - skip - 1 {
+            let curr = row[c];
+            match curr {
+                b'^' => {
+                    map_counts[c - 1] += map_counts[c];
+                    map_counts[c + 1] += map_counts[c];
+                    map_counts[c] = 0;
                 }
                 _ => (),
             }
         }
     }
-    let timelines = map_counts[map_counts.len() - 1].iter().sum();
+    Some(map_counts.iter().sum())
+}
+
+pub fn part_two_original(input: &str) -> Option<u64> {
+    let map = input.c_map();
+    let width = map[0].len();
+    let mut map_counts = vec![0; width];
+    let mut new_map_counts = vec![0; width];
+    let mut skip = width / 2;
+    for row in map.iter().step_by(2) {
+        for c in skip..row.len() - skip {
+            let prev = map_counts[c];
+            let curr = row[c];
+            match (prev, curr) {
+                (_, 'S') => new_map_counts[c] = 1,
+                (x, '.') if x > 0 => {
+                    new_map_counts[c] += map_counts[c];
+                }
+                (x, '^') if x > 0 => {
+                    new_map_counts[c - 1] += map_counts[c];
+                    new_map_counts[c + 1] += map_counts[c];
+                }
+                _ => (),
+            }
+        }
+        let temp = map_counts;
+        map_counts = new_map_counts;
+        new_map_counts = temp;
+        if skip != 0 {
+            skip -= 1;
+            new_map_counts.iter_mut().for_each(|x| *x = 0);
+        }
+    }
+    let timelines = map_counts.iter().sum();
     Some(timelines)
 }
 
